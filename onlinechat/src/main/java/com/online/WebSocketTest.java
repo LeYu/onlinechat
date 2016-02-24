87c003ba54c0c4a81d161c4f380aef70e7f929ac
du.chen <du.chen@CDC312759.dir.svc.accenture.com>
2016-02-24 18:44:57 +0800

    add join name

--- onlinechat/src/main/java/com/online/WebSocketTest.java
+++ onlinechat/src/main/java/com/online/WebSocketTest.java
@@ -9,13 +9,15 @@ import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
  
 //该注解用来指定一个URI，客户端可以通过这个URI来连接到WebSocket。类似Servlet的注解mapping。无需在web.xml中配置。
-@ServerEndpoint("/websocket")
+@ServerEndpoint("/websocket/{name}")
 public class WebSocketTest {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
+    private String name;
      
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
     private static CopyOnWriteArraySet<WebSocketTest> webSocketSet = new CopyOnWriteArraySet<WebSocketTest>();
@@ -28,8 +30,9 @@ public class WebSocketTest {
      * @param session  可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
     @OnOpen
-    public void onOpen(Session session){
+    public void onOpen(Session session, @PathParam(value = "name") String name){
         this.session = session;
+        this.name = name;
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
@@ -57,7 +60,7 @@ public class WebSocketTest {
         //群发消息
         for(WebSocketTest item: webSocketSet){             
             try {
-                item.sendMessage(message);
+                item.sendMessage(this.name + ":" +message);
             } catch (IOException e) {
                 e.printStackTrace();
                 continue;
@@ -97,4 +100,14 @@ public class WebSocketTest {
     public static synchronized void subOnlineCount() {
     	WebSocketTest.onlineCount--;
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
 }
